6a418a5daa38e8c8a549de9019caa353b6d190ac
VAIBHAV BHARDWAJ <167551381+bhardwajjvaibhav@users.noreply.github.com>
2025-07-21 09:58:01 +0530

    Time: 15 ms (14.77%), Space: 19.7 MB (19.14%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,22 +3,21 @@ class Solution:
         n=len(nums)
         count=0
 
-        for i in range(n):
+        for i in range(len(nums)):
             if count==0:
                 count+=1
                 el=nums[i]
-            
-            elif nums[i]==el:
+            elif el==nums[i]:
                 count+=1
-
+            
             else:
                 count-=1
-        count+=1
-        for i in range(n):
+
+        for i in range(len(nums)):
+            
             if nums[i]==el:
                 count+=1
-        
         if count>n/2:
             return el
         else:
-             return 0
+            return 0
